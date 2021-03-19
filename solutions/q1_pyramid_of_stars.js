cf08b7fc1cbf7d3cb7052994c4451d159b1f3def
Aiswaryakv97 <aiswaryakviyer@gmail.com>
2021-03-19 15:03:41 +0530

    added

---
+++ solutions/q1_pyramid_of_stars.js
@@ -0,0 +1,19 @@
+/* Write a program to build a `Pyramid of stars` of given height */
+
+const buildPyramid = () => {
+	// Write my code here
+};
+
+/* For example,
+INPUT - buildPyramid(6)
+OUTPUT -
+     *
+    * *
+   * * *
+  * * * *
+ * * * * *
+* * * * * *
+
+*/
+
+module.exports = buildPyramid;
